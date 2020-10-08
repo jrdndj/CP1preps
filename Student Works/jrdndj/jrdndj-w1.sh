238ddbf94cbe556ec59ba35e686d02b8a8a7b602
jrdndj <jordan.deja@dlsu.edu.ph>
2020-10-08 20:50:37 +0200

    added sample for tomorrow's demo

---
+++ Student Works/jrdndj/jrdndj-w1.sh
@@ -0,0 +1,21 @@
+sample bash scripts
+
+run terminal
+
+ls - list the content of the directory user is currently in
+man [command] - information on how to use [command]
+info [command] - information on how to use [command]
+whatis [command] - short description of the command
+ls [directory] - list the content of the [directory]
+ls -al [directory] - list the content of the [directory], -a: do not ignore entries starting with ., -l: use a long listing format
+pwd - shows the path of the current directory
+cd [directory] - move to the [directory]
+mkdir [directory] - make new directory with name [directory]
+rm -r [directory] - delete directory [directory] with all of its content
+touch [file] - create a new file with name [file]
+cat >[file] - create a new file with name [file] and copy any characters you type at the keyboard to the designated file
+cp [file] [directory] - copy [file] to the [directory]
+rm [file] - delete the file
+more [file] - show the content of the [file]
+mv [file] [target] - rename [file] to [target] OR move [file] to [target]
+chmod [permissions] [file/directory] - change permissions 
