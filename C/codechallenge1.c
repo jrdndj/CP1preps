b48df557cd373b7c5b6def5ed80921c91daeca68
Jordan Aiko Deja <jordan.deja@famnit.upr.si>
2020-11-27 12:12:56 +0100

    Create codechallenge1.c

---
+++ C/codechallenge1.c
@@ -0,0 +1,12 @@
+/* Write a C program that will do the following
+allow the user to input a float number (hint: instead of %d, use %f for scanf. then for printf use %.2f)
+assume that the values input is in degree celsius
+the program will then print its equivalent value in degree fahrenheit and in degree Kelvin
+you may google the formula but the code to convert it must be written by you. 
+print all three values. 
+use interactive input, use comments. 
+in the comments, indicate your source link of the formula */
+
+#include<stdio.h>
+
+//begin here
