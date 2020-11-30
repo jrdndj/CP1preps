2b58dd0d4c1ff80a58e080cc5b931404c514ce2d
Jordan Aiko Deja <jordan.deja@famnit.upr.si>
2020-11-30 08:59:00 +0100

    Create codechallenge02.c

---
+++ C/codechallenge02.c
@@ -0,0 +1,18 @@
+/* 
+A C program that will allow the user to input integers. The integer input can be a 2,3 or 4-digit integer. Reject if it is a 1 digit integer. Reject if it is 5-digits or more. 
+Integers must be positive. 
+If the integer has "23" in it, print "Jordan"
+If the integer has "24" in it, print "Kobe"
+Else, print "useless". 
+
+sample inputs and outputs: 
+23 Jordan
+ 24 Kobe
+1024 Kobe
+5231 Jordan
+2400 Kobe
+5555 useless
+55 useless
+555 useless
+*/
+#include<stdio.h>
