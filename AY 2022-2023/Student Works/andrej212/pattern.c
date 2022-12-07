f9898c42baf87b2c8ef12f445e34133505fd876a
andrej212 <119525398+andrej212@users.noreply.github.com>
2022-12-07 19:32:44 +0100

    Add files via upload

---
+++ AY 2022-2023/Student Works/andrej212/pattern.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main{
+int dN=5;
+for(int i=dN; i<0; i--){
+	printf("\n *");
+for(int j=0; j<dN; j++){
+	printf("\n ");
+	for(){
+		printf("\n");
+	}
+}
+}
+
+
+
+
+
+
+return 0;
+}
