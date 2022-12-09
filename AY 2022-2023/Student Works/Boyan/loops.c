367f9d6693d8abb4ba9bc651a1872cad3b8ff120
SudoSensei18 <119048785+SudoSensei18@users.noreply.github.com>
2022-12-09 13:30:40 +0100

    Add files via upload

---
+++ AY 2022-2023/Student Works/Boyan/loops.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]){
+
+	//declaring variables
+	int dFactor=0;
+
+	for (; dFactor <=10; dFactor++){
+		/* code */
+	
+	//print equation
+	printf("\n %dx%d= %d", dFactor, dFactor, dFactor*dFactor);
+	}// end for loop
+	printf("\n");
+	return 0;
+}
