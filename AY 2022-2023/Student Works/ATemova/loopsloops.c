5290c750d7c3d5ea09a52653911e6ef4605b093a
ATemova <119525418+ATemova@users.noreply.github.com>
2022-12-09 17:07:40 +0100

    Create loopsloops.c

---
+++ AY 2022-2023/Student Works/ATemova/loopsloops.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	//declare a variable
+	int dFactor1=1;
+
+	for(;dFactor1<=10; dFactor1++){
+	//print the statement
+	printf("\n %d x %d = %d", dFactor1, dFactor1, dFactor1*dFactor1);
+	}//endfor	
+	return 0;
+}
