8f3d6d16dc5cf5c0ff7cf4c0dec5108fbe5eeca8
aleksaradovic23 <119056163+aleksaradovic23@users.noreply.github.com>
2022-12-09 15:02:17 +0100

    Create arrays.c
    I will dream about arrays for the rest of my life

---
+++ AY 2022-2023/Student Works/AleksaRAdovic/arrays.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	int dArr[10], dOddCtr=0;
+
+	for(int dCtr = 0; dCtr<10; dCtr++){
+		printf("\n Please enter the number for slot %d", dCtr);
+		scanf("%d", &dArr[dCtr]);
+	}
+	printf("\n");
+	for(int dCtr=0; dCtr<10; dCtr++){
+		printf("%d ", dArr[dCtr]);
+		if( dArr[dCtr]%2!=0){
+			dOddCtr++;
+		}//end if
+	}//end forloop
+	printf("\n There are %d odd numbers in your array \n", dOddCtr);
+
+	return 0;
+}
