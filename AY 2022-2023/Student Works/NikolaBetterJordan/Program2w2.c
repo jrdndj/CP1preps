f737bf8d0ab8402b6650c8956d599e80cb41c8be
NikolaJord <119525382+NikolaJord@users.noreply.github.com>
2022-12-09 15:15:14 +0100

    Add files via upload

---
+++ AY 2022-2023/Student Works/NikolaBetterJordan/Program2w2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int dFactor1, dFactor2;
+	for (dFactor1=1; dFactor1<=10; dFactor1++){
+		for(dFactor2=10; dFactor2>0; dFactor2--){
+			printf(" %d ", dFactor1*dFactor2);
+		}
+		printf("\n");
+	}
+
+	return 1;
+}
