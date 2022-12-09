563bc14993d102a44bce692d1c4ebe7c08eb5e35
frostcouchy <119669374+frostcouchy@users.noreply.github.com>
2022-12-09 15:08:21 +0100

    Add files via upload

---
+++ AY 2022-2023/Student Works/aleksaalijevski/prog4.c
@@ -0,0 +1,18 @@
+//this program prints 1x1 all the way until 10x10
+#include <stdio.h>
+int main(){
+    //declare variable
+
+    int dFactor=1;
+//int dFactor=1;=;
+    for(; dFactor<=10; dFactor++){
+    printf("%d x %d = %d \n",dFactor,dFactor,dFactor*dFactor);
+   
+
+
+    }//end for
+
+
+
+    return 0;
+}
