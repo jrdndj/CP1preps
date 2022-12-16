391f1e67ce452fbed0a0bdbd9401542ab0b853f9
EkaterinaB29 <119041374+EkaterinaB29@users.noreply.github.com>
2022-12-16 11:44:37 +0100

    Add files via upload

---
+++ AY 2022-2023/Student Works/ekaterina/practisestrings.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+
+// index: 0 1 2 3 4 
+// mydata:a b c d \0
+ int main()
+ {
+ 	char mydata[5];
+ 	mydata[0]= 'a';
+ 	mydata[1]='b';
+ 	mydata[2]='c';
+ 	mydata[3]='d';
+ 	mydata[4]='\0';
+
+ 	printf("mydata[2] = %c\n",mydata[2]);
+ 	printf("\n%s\n", mydata);
+
+ 	char s[]="this is my string.";
+ 	//adds place for the 
+
+ 	for(int i=0;i<19;i++)
+ 	{
+ 		if(s[i]=='\0')
+ 	printf("s[%d] = nt\n", i); //this is spevcial blank 
+ 	} //null terminator 
+ 	else{
+ 	printf("s[%d] = %c\n",i,s[i]);
+ 	}
+ 	printf("\n%s\n",s);
+
+ 	chars
+ 	printf("Enter:")
+
+
+
+ return 0;
+ }
