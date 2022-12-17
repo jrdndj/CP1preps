113b83742862021473918af8e66fdb2567e8e980
EkaterinaB29 <119041374+EkaterinaB29@users.noreply.github.com>
2022-12-17 22:11:54 +0100

    Update vowel.c

--- AY 2022-2023/Student Works/ekaterina/vowel.c
+++ AY 2022-2023/Student Works/ekaterina/vowel.c
@@ -1,21 +1,3 @@
-/*	1.a program to know if a letter is a vowel or not 
-	2.count number of vowels in a string
-	3.get the length of a string without using string functions
-	4.count number of words in a sentence 
-	5.count number of times a letter is repeated in a string
-	6.calculate frequency of a number 
-	7.convert upper to lowercase and vise versa
-	8.print number of white space in a string
-	9.the check if a given string is a palindrome or not 
-	10.print a reverse of a string
-	11.sum of digitis in a string
-	12.find the first capital letter in a string
-	13.split sentence for a given character 
-	14.sort names in Alphabetical odrer
-	15.reverse of words in a given string
-	16.print largest and smallest word from given string 
-
-	*/
 
 #include <stdio.h>
 #include <string.h>
