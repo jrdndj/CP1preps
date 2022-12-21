f9b9fe998053d7dcd239c6bc54df1ea686aa03dc
janaStani <119035956+janaStani@users.noreply.github.com>
2022-12-21 11:38:47 +0100

    Add files via upload

--- AY 2022-2023/Student Works/jananaa/problem002.c
+++ AY 2022-2023/Student Works/jananaa/problem002.c
@@ -3,23 +3,22 @@ int main()
 {
 
   int number,counter=0, sum = 0, avg;
-  printf("Enter a number: ");
+  //printf("Enter a number: ");
   
-  do {
+
+ while(number != 0){
+  printf("Enter a number: ");
     scanf("%d", &number);
     sum += number;
     
     counter++;
-  
-  }
-  while(number != 0);
+    }
 
   printf("Sum = %d\n",sum);
-  if (counter == 0)
-  	printf("No average!");
+  if (counter == 0){
+  printf("No average!");}
   else{
   avg = sum / counter;
   printf("Average = %d\n", avg);
-}
-  return 0;
+  return 0;}
 }
