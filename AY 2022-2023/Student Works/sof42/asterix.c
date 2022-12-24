4384ece6e7f62d4c6713f00f8760af161270fa70
sof42 <119482801+sof42@users.noreply.github.com>
2022-12-24 11:16:31 +0100

    Update asterix.c

--- AY 2022-2023/Student Works/sof42/asterix.c
+++ AY 2022-2023/Student Works/sof42/asterix.c
@@ -1,14 +1,23 @@
-#include<stdio.h>
-
+#include <stdio.h>
 int main(){
 	//declare variables
-	int dStar;
-	printf("\n Please enter a value between 1 and 10: ");
-	scanf("%d", &dStar);
-	for(int i = dStar; i>=1; i--){
-		for(int j = 1; j<=i; j++){
-			printf("*");
-		}//endinnerfor
+	int counter;
+	printf("\nEnter the maximum number of asterixes: ");
+
+	//prompt the user for input
+	scanf("%d", &counter);
+
+	printf("\nThey look like this:\n\n");
+	for (int i = 0; i<=counter; i++ ){
+		for (int j = 0; j <= counter; j++){
+			if(j>i){
+				printf("*");
+			}//endif
+			else{
+				printf(" ");
+			}//endelse
+		}//endfor2
 		printf("\n");
-	}//endouterfor
+	}//endfor1
+	return 0;
 }//endmain
