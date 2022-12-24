2dab6b78741fef3e78b3620a0eaee645aed81941
Kristina <117029938+krisssttinaa@users.noreply.github.com>
2022-12-24 18:08:31 +0100

    Update Kristina_Piiarska_problem00.c

--- AY 2022-2023/Student Works/krisssttinaa/Kristina_Piiarska_problem00.c
+++ AY 2022-2023/Student Works/krisssttinaa/Kristina_Piiarska_problem00.c
@@ -6,7 +6,7 @@ int main() {
   int dStatus;
 do {
   //ask the user for the type of animal
-  printf("Enter the type of animal (D for dog, C for cat, B for bird, R for reptile): ");
+  printf("Enter the type of animal (D or d for dog, C or c for cat, B or b for bird, R or r for reptile): ");
   scanf("%c", &cAnimal);
 
   //determine the insurance fee based on the type and neutering status of the animal
@@ -26,10 +26,10 @@ do {
     if (cNeutered == 'Y' || cNeutered == 'y') {
       printf("\n Insurance fee for a cat that has been neutered costs $40.00");
     } else {
-      printf("\n Insurance fee for a dog that has not been neutered costs $60.0");
+      printf("\n Insurance fee for a cat that has not been neutered costs $60.0");
     }
   } else if (cAnimal == 'B' || cAnimal == 'b' || cAnimal == 'R' || cAnimal == 'r') {
-    printf("\n Insurance fee for a cat that has been neutered costs $0.00");
+    printf("\n Insurance fee is $0.00");
   } else {
     printf("Error: Unrecognized animal type. Please, insert another type of animal.\n");
   }
