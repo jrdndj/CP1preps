e27099ff73b9377cfb9ae7b5f5e0829508fa0a7f
edameska <119482820+edameska@users.noreply.github.com>
2023-01-03 20:42:40 +0100

    Added basements

--- AY 2022-2023/Student Works/edameska/Homework/task1/main.c
+++ AY 2022-2023/Student Works/edameska/Homework/task1/main.c
@@ -1,7 +1,21 @@
-//task1
 #include <stdio.h>
 #include <string.h>
-//for minimum of the grade
+#include <stdbool.h> //for bool
+
+//added basements
+
+//user input
+bool ask(char answer)
+{
+	printf("Do you want to alight the elevator? Write y or n.\n");
+	scanf("%c",&answer);
+	getchar();
+	if(answer!='y') {
+		printf("Goodbye\n");
+		return false;
+	}//end if
+	else return true;	
+}//end function
 
 //if the lift is going up
 void GoingUp(int n,int m){
@@ -11,7 +25,7 @@ void GoingUp(int n,int m){
 		printf(".\n");
 		printf(".\n");
 	}//end for
-}
+}//end function
 
 //if the lift is going down
 void GoingDown(int n,int m){
@@ -21,106 +35,125 @@ void GoingDown(int n,int m){
 		printf(".\n");
 		printf(".\n");
 	}//end for
-}
+}//end function
 
 int main()
 {	//declaring variables
-	char GoingTo;
-	char answer;
 	int Current=0;
-	
+	char GoingTo[2];
+	bool answer,answerForRealz;
+
 	//asling for user input
 	printf("You are on the ground floor\n");
-	printf("Do you want to alight the elevator? Write y or n.\n");
-	scanf("%c",&answer);
-	getchar();
-	if(answer!='y') {
-		printf("Goodbye\n");
-	}//end if
+	answerForRealz=ask(answer);	
 
 //while loop for multiple elevator rides
-while(answer=='y') {
+while(answerForRealz==true) {
+
+	//asking for user input
 	printf("Where do you want to go?\n");
-	scanf("%c",&GoingTo);
+	scanf("%s",GoingTo);
 	getchar();
-	
-	//switch because it was easier to manage than if
-	switch(GoingTo){
+//separates basemments from other floors since switch doesn't work with strings
+	if(GoingTo[0]=='B'){
+		//this way the if statements only have to look at the second character
+		if(GoingTo[1]=='1'){
+			if(Current>-1)
+			{
+				GoingDown(Current,-1);
+			}//end if
+			if(Current<-1){
+				GoingUp(Current,-1);
+			}//end if
+		printf("You are in Basement 1\n");
+		Current=-1;
+		}//end basement 1 if
+
+		else if(GoingTo[1]=='2'){
+			if(Current>-2)
+			{
+				GoingDown(Current,-2);
+			}//end if
+			if(Current<-2){
+				GoingUp(Current,-2);
+			}//end if
+		printf("You are on Basement 2\n");
+		Current=-2;
+		}//end basement 2 if
+
+		else if(GoingTo[1]=='3'){
+			GoingUp(Current,-3);
+		printf("You are in Basement 3\n");
+		Current=-3;
+		}//end basement 3 if
+
+	}//end basements if
+
+	else{//switch case looks at the floors with 1 char
+	switch(GoingTo[0]){
 	case 'G':
 		GoingDown(Current,0);
 		printf("You are on the ground floor\n");
 		Current = 0;
 		break;
-          
 	case '1':
-		if(Current>(int)answer)
+		if(Current>(int)GoingTo[0])
 		{
 			GoingDown(Current,1);
 		}//end if
-		if(Current<(int)answer){
+		if(Current<(int)GoingTo[0]){
 			GoingUp(Current,1);
 		}//end if
 		printf("You are on the 1st floor\n");
 		Current=1;
 		break;
-          
 	case '2':
-		if(Current>(int)answer)
+		if(Current>(int)GoingTo[0])
 		{
 			GoingDown(Current,2);
 		}//end if
-		if(Current<(int)answer){
+		if(Current<(int)GoingTo[0]){
 			GoingUp(Current,2);
 		}//end if
 		printf("You are on the 2nd floor\n");
 		Current=2;
 		break;
-          
 	case '3':
-		if(Current>(int)answer)
+		if(Current>(int)GoingTo[0])
 		{
 			GoingDown(Current,3);
 		}//end if
-		if(Current<(int)answer){
+		if(Current<(int)GoingTo[0]){
 			GoingUp(Current,3);
 		}//end if
 		printf("You are on the 3rd floor\n");
 		Current=3;
 		break;
-          
 	case '4':
-		if(Current>(int)answer)
+		if(Current>(int)GoingTo[0])
 		{
 			GoingDown(Current,4);
 		}// end if
-		if(Current<(int)answer){
+		if(Current<(int)GoingTo[0]){
 			GoingUp(Current,4);
 		}//end if
 		printf("You are on the 4th floor\n");
 		Current=4;
 		break;
-          
 	case '5':
 		GoingUp(Current,5);
 		printf("You are on the 5th floor\n");
 		Current=5;
 		break;
-          
 	default:
 		printf("Invalid input\n");
 		break;
 	}//end switch
-	
-//asking for user input
-printf("Do you want to alight the elevator?\n");
-	scanf("%c",&answer);
-	getchar();
-	if(answer!='y') {
-		printf("Goodbye\n");
-		break;
-	}//end if
+	}//end else
+	answerForRealz=ask(answer);		
 
- }//end while	
+ }//wnd while
+	
 
 	return 0;
 }
