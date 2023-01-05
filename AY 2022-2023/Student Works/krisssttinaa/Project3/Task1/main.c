1ca75c7d299eab67c78915c9a0df317ec43ed9f0
Kristina <117029938+krisssttinaa@users.noreply.github.com>
2023-01-05 12:23:18 +0100

    Update main.c Task1
    Now elevator moves B3, B2, B1, G, 1, 2, 3, 4, 5

--- AY 2022-2023/Student Works/krisssttinaa/Project3/Task1/main.c
+++ AY 2022-2023/Student Works/krisssttinaa/Project3/Task1/main.c
@@ -1,57 +1,120 @@
 #include <stdio.h>
-// Function to move the elevator to the specified floor
-void moveElevator(int dCurrentFloor, int dDesiredFloor){
-    if (dCurrentFloor < dDesiredFloor){
-        //for loop to print the floors we are passing by, when going up
-        printf("Going up: ");
-        for (int i = dCurrentFloor + 1; i <= dDesiredFloor; i++){
-            printf("%d\n", i);
-        }
-        printf("Arrived on %d floor\n", dDesiredFloor);
+//Library for a Boolean data type (to use true in while loop)
+#include <stdbool.h>
+//library for additional funtions
+#include <ctype.h>
+//library for characters-string
+#include <string.h>
+
+//Declaration of a function (prototype)
+//To move from one floor to another
+void moveTo(int currentFloor, int desiredFloor);
+//To check if the user's input is valid
+char* checkIfValid(int* desiredFloor);
+void changeType(char* sDesiredFloor, int* dDesiredFloor);
+
+int main() {
+  char sCurrentFloor[3] = "G";
+  int dCurrentFloor = 0;
+
+  while (true) {
+    printf("Current floor: %s\n", sCurrentFloor);
+    printf("Enter a destination floor (B3, B2, B1, G, 1, 2, 3, 4, 5): ");
+
+    int dDesiredFloor = 0;
+    char sDesiredFloor[3] = "";
+    strcpy(sDesiredFloor, checkIfValid(&dDesiredFloor));
+
+    moveTo(dCurrentFloor, dDesiredFloor);
+
+    dCurrentFloor = dDesiredFloor;
+    changeType(sDesiredFloor, &dDesiredFloor);
+    strcpy(sCurrentFloor, sDesiredFloor);
+
+    printf("Do you want to alight? (y/n) ");
+    char cContinue;
+    scanf(" %c", &cContinue);
+
+    if (cContinue == 'y' || cContinue == 'Y') {
+      printf("Thanks for using the elevator.\nI hope you enjoyed and saved your time!");
+        break;
     }
-    else if (dCurrentFloor > dDesiredFloor){
-        //for loop to print the floors we are passing by, when going down
-        printf("Going down: ");
-        for (int i = dCurrentFloor - 1; i >= dDesiredFloor; i--){
-            printf("%d\n", i);
-        }
-        printf("\nArrived on %d floor\n", dDesiredFloor);
-    }
-    else if (dCurrentFloor == dDesiredFloor) {
-        printf("You are already on %d floor. Choose another floor to go.\n", dDesiredFloor);
-    }
-}
-int main(){
-  int dCurrentFloor = 0; // current floor of the elevator
-  int dDestinationFloor; // destination floor for the elevator
-
-  printf("Welcome to the elevator simulator!\n");
-  printf("Current floor: %d\n", dCurrentFloor);
-
-  // Keep looping until the user chooses to exit
-  while (1){
-    printf("\nEnter destination floor (0-5, or 6 to exit the elevator): ");
-    scanf("%d", &dDestinationFloor);
-
-    // If the user wants to exit, break out of the loop
-    if (dDestinationFloor == 6){
-      break;
-    }
-
-    // Validate the input
-    if (dDestinationFloor < 0 || dDestinationFloor > 5){
-      printf("Invalid input. Please try again.\n");
-      continue;
-    }
-
-    // Move the elevator to the destination floor
-    moveElevator(dCurrentFloor, dDestinationFloor);
-
-    // Update the current floor
-    dCurrentFloor = dDestinationFloor;
-    }
-
-  printf("Thanks for using elevator.\nI hope you enjoyed and saved your time!\n");
+  }
 
   return 0;
+}
+
+void moveTo(int dCurrentFloor, int dDesiredFloor) {
+  if (dCurrentFloor == dDesiredFloor) {
+    printf("You are already on this floor.\n");
+    return;
+  }
+
+  if (dCurrentFloor < dDesiredFloor) {
+    printf("Going up: ");
+    for (int i = dCurrentFloor + 1; i <= dDesiredFloor; i++) {
+      printf("%d ", i);
+    }
+  }
+  else {
+    printf("Going down: ");
+    for (int i = dCurrentFloor - 1; i >= dDesiredFloor; i--) {
+      printf("%d ", i);
+    }
+  }
+  printf("\n");
+}
+
+char* checkIfValid(int* desiredFloor) {
+  //Address of stack memory associated with local variable 'sDesiredFloor' returned
+  //so, I add static
+  //this will allocate space for matches in the static space and not on the stack
+  static char sDesiredFloor[3];
+  scanf("%s", sDesiredFloor);
+
+  if (strcmp(sDesiredFloor, "B3") == 0) {
+    *desiredFloor = -3;
+    return sDesiredFloor;
+  }
+  else if (strcmp(sDesiredFloor, "B2") == 0) {
+    *desiredFloor = -2;
+    return sDesiredFloor;
+  }
+  else if (strcmp(sDesiredFloor, "B1") == 0) {
+    *desiredFloor = -1;
+    return sDesiredFloor;
+  }
+  else if (strcmp(sDesiredFloor, "G") == 0) {
+    *desiredFloor = 0;
+    return sDesiredFloor;
+  }
+  else if (isdigit(sDesiredFloor[0]) && sDesiredFloor[1] == '\0') {
+    *desiredFloor = sDesiredFloor[0] - '0';
+    return sDesiredFloor;
+  }
+  else {
+    printf("Invalid input. Please enter a valid destination floor (B3, B2, B1, G, 1, 2, 3, 4, 5): ");
+    return checkIfValid(desiredFloor);
+  }
+}
+
+void changeType(char* sDesiredFloor, int* dDesiredFloor) {
+  if (*dDesiredFloor == -3) {
+    sDesiredFloor[0] = 'B';
+    sDesiredFloor[1] = '3';
+  }
+  else if (*dDesiredFloor == -2) {
+    sDesiredFloor[0] = 'B';
+    sDesiredFloor[1] = '2';
+  } 
+  else if (*dDesiredFloor == -1) {
+    sDesiredFloor[0] = 'B';
+    sDesiredFloor[1] = '1';
+  }
+  else if (*dDesiredFloor == 0) {
+    sDesiredFloor[0] = 'G';
+  }
+  else {
+    sDesiredFloor[0] = *dDesiredFloor + '0';
+  }
 }
