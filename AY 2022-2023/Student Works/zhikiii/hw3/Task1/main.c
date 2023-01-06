54dda09def17a03e9f6340d0ae7fc3c980afa2e4
Zhivko Stoimchev <62955474+zstoimchev@users.noreply.github.com>
2023-01-06 22:51:25 +0100

    finished task1, hopefully..
    added functionality for more people to enter and exit the elevator, with limit of 10 people including the user. There is a way also to allow each user to select floor that they want to go, but I left that as an extracurricular activity.. :|

--- AY 2022-2023/Student Works/zhikiii/hw3/Task1/main.c
+++ AY 2022-2023/Student Works/zhikiii/hw3/Task1/main.c
@@ -2,15 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-//function to check if the entered floor is found in the list
-int check_floor(char sArr[][3], char sInput[])
-{
+// function to check if the entered floor is found in the list
+int check_floor(char sArr[][3], char sInput[]) {
     int dFlag = 1;
-    for (int i = 0; i < 9; i++) //I use 9 because there are totally 9 floors, from b3 to 5
-    {
-        if (strcmp(sInput, sArr[i]) == 0) //comparing the floors with the input, return 0 if they are the same
-        {
-            dFlag = 0;   //keeping track if the condition is true or false
+    for (int i = 0; i < 9; i++) { // I use 9 because there are totally 9 floors, from b3 to 5, including g
+        if (strcmp(sInput, sArr[i]) == 0) { // comparing the floors with the input, return 0 if they are the same
+            dFlag = 0; // keeping track if the condition is true or false
             break;
         }
     }
@@ -18,66 +15,95 @@ int check_floor(char sArr[][3], char sInput[])
 }
 
 // return the index of the floor
-int return_index(char sArr[][3], char sFloorToCheck[])
-{
+int return_index(char sArr[][3], char sFloorToCheck[]) {
     int dIndex = 0;
     for (int i = 0; i < 9; i++)
-        if (strcmp(sFloorToCheck, sArr[i]) == 0)
-        {
+        if (strcmp(sFloorToCheck, sArr[i]) == 0) {
             dIndex = i;
             break;
         }
-
     return dIndex;
 }
 
-int main()
-{
-    char sCurrentFloor[2] = "G";    //floor which we currently are
-    char sNextFloor[2];             //floor which the user wants to go
+int main() {
+    // declaring variables until next ==================================================================================
 
-    //possible floors from which the user can select
-    //I use matrix, because sor basement level I need 2 characters, 1 to 3, and there are totally 9 floors
+    char sCurrentFloor[2] = "G"; // floor which we currently are
+    char sNextFloor[2];          // floor which the user wants to go
+
+    // possible floors from which the user can select
+    // I use matrix, because sor basement level I need 2 characters, 1 to 3, and there are totally 9 floors
     char sFloors[9][3] = {"B3\0",
-                         "B2\0",
-                         "B1\0",
-                         "G\0",
-                         "1\0",
-                         "2\0",
-                         "3\0",
-                         "4\0",
-                         "5\0"};
+                          "B2\0",
+                          "B1\0",
+                          "G\0",
+                          "1\0",
+                          "2\0",
+                          "3\0",
+                          "4\0",
+                          "5\0"};
+    int dCurrentNumOfPeople = 1; // number of people in the elevaotr including the current user
+    int dPeopleThatLeft;         // people that left the elevator
+    char cAnswer;                // answer if there are more people to join the elevator
+    int dMaxNumOfPeople = 0;     // maximum people that allows the elevator, max is 10
+    // finished declaring variables =====================================================================================
+    // this is infinite loop, never stops, as real elevator is always expecting a command
 
-    //this is infinite loop, never stops, as real elevator is always expecting a command
-    while (1)
-    {
-        //asking the user for input
+    int flag = 1; // until true, the loop will continue. It will change to false once there are no people in the lift
+    while (flag) {
+        // asking the user for input
         printf("You are currently on floor %s. Which floor you you want to go?: ", sCurrentFloor);
         scanf(" %s", sNextFloor);
-        //with this condition, we check whether the user entered correct input, so the program doesn't crash
-        if (check_floor(sFloors, sNextFloor) == 1)
-        {
+        // with this condition, we check whether the user entered correct input, so the program doesn't crash
+        if (check_floor(sFloors, sNextFloor) == 1) {
             printf("Wrong input, try again. ");
-            continue;   //continue means skip everything below and start the loop over
-        } // endif
+            continue; // continue means skip everything below and start the loop over
+        }             // endif
 
-        //printing whether the lift goes up or down, using the function above
-        if (return_index(sFloors, sNextFloor) > return_index(sFloors, sCurrentFloor))
-        {
+        /* people entering the elevator */
+        cAnswer = 'y';
+        while (cAnswer == 'y') { // no more than 10 people are allowed
+            printf("Are there more people that want to enter the elevator? (y/n): ");
+            scanf(" %c", &cAnswer);
+            if (cAnswer == 'y') {
+                dCurrentNumOfPeople++;
+                dMaxNumOfPeople++;
+                if (dMaxNumOfPeople == 9) { // check if there are still places for more people
+                    printf("The elevator is full!\n");
+                    break;
+                }//endif
+            } // endif
+        }     // endwhile
+
+        /* printing whether the lift goes up or down */
+        if (return_index(sFloors, sNextFloor) > return_index(sFloors, sCurrentFloor)) {
             for (int i = return_index(sFloors, sCurrentFloor) + 1; i <= return_index(sFloors, sNextFloor); i++)
-                printf(" Going up. We reached floor %s.\n", sFloors[i]);
+                printf("\tGoing up. We reached floor %s.\n", sFloors[i]);
         } // endif
-        else
-        {
+        else {
             for (int i = return_index(sFloors, sCurrentFloor); i >= return_index(sFloors, sNextFloor); i--)
-                printf(" Going down. We reached floor %s.\n", sFloors[i]);
+                printf("\tGoing down. We reached floor %s.\n", sFloors[i]);
         } // endelse
-        // printf("You reached floor %s.\n", sNextFloor);
 
-         //remembering the last aligned floor, so the next time we ride we start from that floor
+        /* remembering the last aligned floor, so the next time we ride we start from that floor */
         strcpy(sCurrentFloor, sNextFloor);
 
-        
+        /* people exiting from the elevator */
+        dPeopleThatLeft = -1; // preventing the user from entering wrong input
+        while (dPeopleThatLeft < 0 || dPeopleThatLeft > dCurrentNumOfPeople) {
+            printf(" On floor %s arrived %d people. How many people want to exit? (0-%d): ", sCurrentFloor, dCurrentNumOfPeople, dCurrentNumOfPeople);
+            scanf("%d", &dPeopleThatLeft);
+        }
+        if (dPeopleThatLeft >= dCurrentNumOfPeople) {
+            printf(" There are no more people in the elevator. Goodbye :)\n");
+            flag = 0;
+        } // endif
+        else {
+            dCurrentNumOfPeople -= dPeopleThatLeft; // releasing number of current people
+            dMaxNumOfPeople += dPeopleThatLeft;     // freeing more space for new people to join
+            printf(" There are %d people left in the elevator.\n", dCurrentNumOfPeople);
+        } // endelse
+
     } // endwhile
     return 0;
 } // endmain
